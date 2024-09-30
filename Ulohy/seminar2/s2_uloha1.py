d06f30507f1ccefe1464db261f4502e2b87043a9
jankwa <halienovajana@gmail.com>
2024-09-30 22:03:22 +0200

    Update
    - s2_uloha1 done

--- Ulohy/seminar2/s2_uloha1.py
+++ Ulohy/seminar2/s2_uloha1.py
@@ -1,1 +1,17 @@
 # Nalezení maxima v seznamu čísel (bez použití funkce max())
+
+def maxNumber(n_numbers: list) -> int:
+    max_num = n_numbers[0]
+    for i in range(1, len(n_numbers)):
+        if n_numbers[i] > max_num:
+            max_num = n_numbers[i]
+    return max_num
+
+num_of_nums = int(input("> How many numbers do you want to add?: "))
+numbers = []
+for i in range(0, num_of_nums):
+    item_num = int(input("> Number: "))
+    numbers.append(item_num)
+
+print(numbers)
+print(maxNumber(numbers))
