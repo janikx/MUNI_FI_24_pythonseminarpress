dc85f8e9acebb2e5b4420c2cdce803ab844a91a7
janka_x <79253266+janikx@users.noreply.github.com>
2024-10-01 11:28:05 +0200

    Update s2_uloha2.py
    - s2_uloha2 done

--- Ulohy/seminar2/s2_uloha2.py
+++ Ulohy/seminar2/s2_uloha2.py
@@ -1,2 +1,17 @@
 # Lineární vyhledávaní hodnoty v seznamu (Pomůcka - wiki)
 # https://en.wikipedia.org/wiki/Linear_search
+
+def numberSearch(n: list) -> int:
+    for i in range(len(numbers)):
+        if n == numbers[i]:
+            print(f"Number {n} was found on position {i} in numbers (list).")
+            continue
+
+searched_number = int(input("> Which number do you want to find?: "))
+numbers = []
+for i in range(0, 1000):
+    item_num = int(input("> Number: "))
+    numbers.append(item_num)
+
+print(numbers)
+print(numberSearch(searched_number))
